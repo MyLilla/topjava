6b492bcac9c053ef285d49edb6f3d0e2f5283f4c
MyLilla <ninakarina.iok@gmail.com>
2024-02-15 13:07:50 +0100

    patch03

--- src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
+++ src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
@@ -1,24 +1,18 @@
 package ru.javawebinar.topjava.web;
 
-import static ru.javawebinar.topjava.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
-
 public class SecurityUtil {
 
-    private static int userId;
+    private static int id = 1;
 
     public static int authUserId() {
-       return userId;
+        return id;
     }
 
-    public static void setAuthUserId(int role) {
-        switch (role){
-            case 1: userId = 1;
-            case 2: userId = 2;
-            default: userId = 1;
-        }
+    public static void setAuthUserId(int id) {
+        SecurityUtil.id = id;
     }
 
     public static int authUserCaloriesPerDay() {
-        return DEFAULT_CALORIES_PER_DAY;
+        return 0;
     }
 }
